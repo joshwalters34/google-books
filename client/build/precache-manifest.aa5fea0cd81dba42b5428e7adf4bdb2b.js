22101374192c1062727c15935e8eceb1bf88da59
JWalters <joshwalters34@gmail.com>
2021-06-16 16:49:12 -0400

    updated server.js to deploy

---
+++ client/build/precache-manifest.aa5fea0cd81dba42b5428e7adf4bdb2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dd09c32f0dca2810dd939b38b22fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adabb2948c0526ad2adf",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "cb1d7b4fd292c05c7370",
+    "url": "/static/css/main.b80217f8.chunk.css"
+  },
+  {
+    "revision": "adabb2948c0526ad2adf",
+    "url": "/static/js/2.6184249a.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.6184249a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1d7b4fd292c05c7370",
+    "url": "/static/js/main.7289f939.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const databaseUrl = "googlebooks";
 const collections = ["books"];
 
 const db = mongojs(databaseUrl, collections);
+const PORT = process.env.PORT || 3001;
 
 db.on("error", error => {
   console.log("Database Error:", error);
